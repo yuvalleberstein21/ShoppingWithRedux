f10b9a5cc02ddb6fbc10a5762b9231bf9869d4e7
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-09-17 23:11:24 +0300

    server routes

--- server/index.js
+++ server/index.js
@@ -3,17 +3,24 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/users');
-// const bodyParser = require('body-parser');
+const authRoute = require('./routes/auth');
+const productRoute = require('./routes/products');
+
+
+const bodyParser = require('body-parser');
 
 dotenv.config();
 app.use(express.json())
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 mongoose.connect(process.env.MONGO_URL
 ).then(() => console.log("Connected to MongoDB")).catch((err) => { console.log(err) });
 
+app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("Backend server is running on port 8000");
